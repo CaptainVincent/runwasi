34f397652553b9e3e8ed84b76715c21afbf5a14f
vincent <vincent@secondstate.io>
2023-02-27 06:21:21 +0800

    Update the wasinn demo and the version of wasmedge used in it

--- demo/utils/setup_wasinn_plugin.sh
+++ demo/utils/setup_wasinn_plugin.sh
@@ -4,31 +4,16 @@
 if [ ! -d "$PWD/libtorch" ]
 then
   export PYTORCH_VERSION="1.8.2"
-  curl -s -L -O --remote-name-all https://download.pytorch.org/libtorch/lts/1.8/cpu/libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip
-  unzip -q "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
-  rm -f "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+  export PYTORCH_ABI="libtorch-cxx11-abi"
+  curl -s -L -O --remote-name-all https://download.pytorch.org/libtorch/lts/1.8/cpu/${PYTORCH_ABI}-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip
+  unzip -q "${PYTORCH_ABI}-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+  rm -f "${PYTORCH_ABI}-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
   sudo sh -c 'echo "$(pwd)/libtorch/lib" > /etc/ld.so.conf.d/libtorch.conf'
 fi
 
 # Download Wasmedge
-if [ ! -d "$PWD/WasmEdge-0.11.1-Linux" ]
-then
-  curl -sLO https://github.com/WasmEdge/WasmEdge/releases/download/0.11.1/WasmEdge-0.11.1-ubuntu20.04_x86_64.tar.gz
-  tar -zxf WasmEdge-0.11.1-ubuntu20.04_x86_64.tar.gz
-  rm -f WasmEdge-0.11.1-ubuntu20.04_x86_64.tar.gz
-  sudo sh -c 'echo "$(pwd)/WasmEdge-0.11.1-Linux/lib" > /etc/ld.so.conf.d/libwasmedge.conf'
-fi
-
-# Download WASINN plugin and extract to wasmedge library download path
-if [ ! -f "$PWD/WasmEdge-0.11.1-Linux/lib/wasmedge/libwasmedgePluginWasiNN.so" ]
-then
-  curl -sLO https://github.com/WasmEdge/WasmEdge/releases/download/0.11.1/WasmEdge-plugin-wasi_nn-pytorch-0.11.1-ubuntu20.04_x86_64.tar.gz
-  tar -zxf WasmEdge-plugin-wasi_nn-pytorch-0.11.1-ubuntu20.04_x86_64.tar.gz
-  rm -f WasmEdge-plugin-wasi_nn-pytorch-0.11.1-ubuntu20.04_x86_64.tar.gz
-  mv libwasmedgePluginWasiNN.so WasmEdge-0.11.1-Linux/lib/wasmedge
-fi
+curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash -s -- -v 0.11.2 --plugins wasi_nn-pytorch
+sudo -E sh -c 'echo "$HOME/.wasmedge/lib" > /etc/ld.so.conf.d/libwasmedge.conf'
 
 sudo ldconfig
-export WASMEDGE_INCLUDE_DIR=$PWD/WasmEdge-0.11.1-Linux/include
-export WASMEDGE_LIB_DIR=$PWD/WasmEdge-0.11.1-Linux/lib
-export WASMEDGE_PLUGIN_PATH=$PWD/WasmEdge-0.11.1-Linux/lib/wasmedge
+export WASMEDGE_PLUGIN_PATH="$HOME/.wasmedge/plugin"
